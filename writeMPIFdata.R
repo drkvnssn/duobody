3b4fb3b48bb077d59d22fc22551b2664b54dc3d5
Dirk van Essen <h.vanessen@vumc.nl>
2017-12-06 22:21:08 +0100

    added function writeMPIFdata

--- writeMPIFdata.R
+++ writeMPIFdata.R
@@ -1,17 +1,23 @@
-writeMPIFdata <- function(data = NULL, file = NULL, path = NULL){
+writeMPIFdata <- function(data = NULL, file = NULL, path = NULL, verbose = TRUE){
   if((class(data)[1] == "MPIFdata") != TRUE){
     stop ("data structure is not in the correct format.\n\n")
   } 
-  if(is.null(file) == TRUE){
-    stop("No probe name was given to update.\n ")
-  }
   if(is.null(path) == TRUE){
-    cat("No data path was given. Using current work directory.\n")
-    print(getwd())
-    path <- test
+    if(verbose == TRUE){
+      cat("No data path was given. Using path in phenoData.\n")
+    }
+    path <- data@phenoData['filepath',1]
   }
-  ###
-
+  if(is.null(file) == TRUE){
+    if(verbose == TRUE){
+      cat("No file name was given. Will generate a generic output file.\n")
+    }
+    file <- paste0("Experiment-results_",data@phenoData['samplename',1],".xlsx")
+  }
+  outputfile <- file.path(path, file)
+  if(verbose == TRUE){
+    cat("Writing Experiment Data to the following location:\n")
+    print(path)
+  }
   write.xlsx(x = data@experimentData, file = outputfile)
- 
 }
