2cf8ac46ecf2c9b8f498d70d33f780ddf6179a6c
Dirk van Essen <h.vanessen@vumc.nl>
2017-12-08 00:31:04 +0100

    added function analyzeProbes()
    minor changes in probeCombn and subsetProbes

---
+++ analyzeProbes.R
@@ -0,0 +1,90 @@
+analyzeProbes <- function(data = NULL, probes = NULL, 
+                           verbose = FALSE){
+  if((class(data)[1] == "MPIFdata") != TRUE){
+    stop ("data structure is not in the correct format.\n\n")
+  } 
+  if(is.null(probe1) == TRUE){
+    stop("Probe 1 for subsetting was not given.\n")
+  }
+  if(is.null(probe1) == TRUE){
+    stop("Probe 2 for subsetting was not given.\n")
+  }
+  if(verbose == TRUE){
+    cat("Creating subsets.\n")
+  }
+  
+  probes <- unique(tolower(probes))
+  
+  ### create subsets
+  if(length(probes) == 2){
+    if(verbose == TRUE){
+      cat("Analyzing two probe combinations.\n")
+    }
+    probe1Column <- grep(colnames(data@assayData), pattern = probes[1], ignore.case = TRUE, fixed = FALSE)
+    probe1 <- colnames(data@assayData)[probe1Column]
+    probe2Column <- grep(colnames(data@assayData), pattern = probes[2], ignore.case = TRUE, fixed = FALSE)
+    probe2 <- colnames(data@assayData)[probe2Column]
+    
+    probe1pos <- subsetProbe(data = data, probe = probes[1], positive = TRUE)
+    probe2pos <- subsetProbe(data = probe1pos, probe = probes[2], positive = TRUE)
+    probe2neg <- subsetProbe(data = probe1pos, probe = probes[2], positive = FALSE)
+    #probe1pos.combn <- .probeCombn(p1 = probe1pos@phenoData['subset',1], p2 = probe2)
+    
+    ### GET RESULTS
+    probe.combinations <- c(paste0(probe1,"+"), 
+                            probe2pos@phenoData['subset',1], 
+                            probe2neg@phenoData['subset',1])
+    
+    results <- matrix(data = NA, nrow = length(probe.combinations), ncol = 2, 
+                      dimnames = list(c(1:length(probe.combinations)), 
+                                      c("Probes","Counts")))
+    results[,1] <- probe.combinations
+    results[1,2] <- data@experimentData['posCount', probe1Column]
+    results[2,2] <- probe1pos@experimentData['posCount', probe2Column]
+    results[3,2] <- probe1pos@experimentData['negCount', probe2Column]
+
+  }
+  if(length(probes) == 3){
+    if(verbose == TRUE){
+      cat("Analyzing three probe combinations.\n")
+    }
+  probe1Column <- grep(colnames(data@assayData), pattern = probes[1], ignore.case = TRUE, fixed = FALSE)
+  probe1 <- colnames(data@assayData)[probe1Column]
+  probe2Column <- grep(colnames(data@assayData), pattern = probes[2], ignore.case = TRUE, fixed = FALSE)
+  probe2 <- colnames(data@assayData)[probe2Column]
+  probe3Column <- grep(colnames(data@assayData), pattern = probes[3], ignore.case = TRUE, fixed = FALSE)
+  probe3 <- colnames(data@assayData)[probe2Column]
+  
+  probe1pos <- subsetProbe(data = data, probe = probes[1], positive = TRUE)
+  probe2pos <- subsetProbe(data = probe1pos, probe = probes[2], positive = TRUE)
+  probe2neg <- subsetProbe(data = probe1pos, probe = probes[2], positive = FALSE)
+  probe2pos3pos <- subsetProbe(data = probe2pos, probe = probes[3], positive = TRUE)
+  probe2pos3neg <- subsetProbe(data = probe2pos, probe = probes[3], positive = FALSE)
+  probe2neg3pos <- subsetProbe(data = probe2neg, probe = probes[3], positive = TRUE)
+  probe2neg3neg <- subsetProbe(data = probe2neg, probe = probes[3], positive = FALSE)
+  
+  probe.combinations <- c(paste0(probe1,"+"), 
+                          probe2pos@phenoData['subset',1], 
+                          probe2neg@phenoData['subset',1],
+                          probe2pos3pos@phenoData['subset',1], 
+                          probe2pos3neg@phenoData['subset',1],
+                          probe2neg3pos@phenoData['subset',1], 
+                          probe2neg3neg@phenoData['subset',1]
+                          )
+  ### GET RESULTS
+  results <- matrix(data = NA, nrow = length(probe.combinations), ncol = 2, 
+                    dimnames = list(c(1:length(probe.combinations)), 
+                                    c("Probes","Counts")))
+  results[,1] <- probe.combinations
+  results[1,2] <- data@experimentData['posCount', probe1Column]
+  results[2,2] <- probe1pos@experimentData['posCount', probe2Column]
+  results[3,2] <- probe1pos@experimentData['negCount', probe2Column]
+  results[4,2] <- probe2pos@experimentData['posCount', probe3Column]
+  results[5,2] <- probe2pos@experimentData['negCount', probe3Column]
+  results[6,2] <- probe2neg@experimentData['posCount', probe3Column]
+  results[7,2] <- probe2neg@experimentData['negCount', probe3Column]
+  }
+  results <- data.frame(results)
+  return(results)
+}
+ 
--- probeCombn.R
+++ probeCombn.R
@@ -7,3 +7,10 @@
     
     return(result)
 }
+.probeCombn <- function(p1, p2){
+  p1p2p <- paste0(p1,", ", p2,"+")
+  p1p2n <- paste0(p1,", ", p2,"-")
+  result <- c(p1p2p, p1p2n)
+  
+  return(result)
+}
