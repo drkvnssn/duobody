82598736e6dbde5090884279650c581949ce82b9
Hendrik_F van_Essen <h.vanessen@vumc.nl>
2017-12-13 09:57:55 +0100

    minor changes

--- shiny/server.R
+++ shiny/server.R
@@ -1,3 +1,4 @@
+# install.packages("//vumc.nl/onderzoek$/s4e-gpfs1/pa-tgac-01/analisten/Dirk/r_scripts/DuoBody_Analysis/shiny_1.0.5.zip", repos = NULL, type = "win.binary", lib = "N:/Documenten/R/win-library/3.2")
 library(shiny)
 
 # 1. load/source the MIPF functions
