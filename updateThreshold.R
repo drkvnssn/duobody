59bf452c7667f0b76d779433b614dec571a90b9a
Hendrik_F van_Essen <h.vanessen@vumc.nl>
2017-12-20 12:58:30 +0100

    added write option to updateThreshold()

--- updateThreshold.R
+++ updateThreshold.R
@@ -1,5 +1,6 @@
 updateThreshold <- function(data = NULL, probe = NULL, threshold = NULL, 
-                            plot = TRUE, col = "red", verbose = TRUE){
+                            plot = TRUE, col = "red", write = FALSE,
+                            verbose = TRUE){
   if((class(data)[1] == "MPIFdata") != TRUE){
     stop ("data structure is not in the correct format.\n\n")
   } 
@@ -9,11 +10,32 @@ updateThreshold <- function(data = NULL, probe = NULL, threshold = NULL,
   if(is.null(threshold) == TRUE){
     stop("No threshold data was given for update.\n ")
   }
+  if(write == TRUE){
+    probes <- colnames(data@experimentData)
+    thresholds <- data@experimentData['threshold',]
+    thresholdData <- matrix(data = cbind(probes, thresholds), 
+                            ncol = 2, byrow = FALSE, 
+                            dimnames = list(c(), c("probe", "threshold")))
+    dateTime <- format(Sys.time(), "%Y%m%d-%H%M%S")
+    filename <- paste0(dateTime,"_",data@phenoData['samplename',], "_thresholds.txt")
+    outputFile <- file.path(data@phenoData['filepath',], filename)
+    write.table(x = thresholdData, file = outputFile, sep = "\t", quote = FALSE, row.names = FALSE)
+  }
   
   probeColumn <- grep(colnames(data@assayData), pattern = probe, ignore.case = TRUE, fixed = FALSE)
   data@experimentData['threshold', probeColumn] <- threshold
   
   data <- .updateMPIFdata(data = data)
+  
+  if(write == TRUE){
+    probes <- colnames(data@experimentData)
+    thresholds <- data@experimentData['threshold',]
+    thresholdData <- matrix(data = cbind(probes, thresholds), 
+                            ncol = 2, byrow = FALSE, 
+                            dimnames = list(c(), c("probe", "threshold")))
+    outputFile <- file.path(data@phenoData['filepath',], data@phenoData['thresholdfile',])
+    write.table(x = thresholdData, file = outputFile, sep = "\t", quote = FALSE, row.names = FALSE)
+  }
   
   if(plot == TRUE){
     plotCells(data = data, probe = probe, posCol = col)
